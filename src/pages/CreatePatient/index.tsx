195d270087d941fdfbe4767314655a2122705a7d
Josué Heringer <josuedelgadoheringer98@gmail.com>
2021-03-08 14:17:02 -0300

    Iniciando patient

---
+++ src/pages/CreatePatient/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AdminLayout from "../../layouts/Admin";
+const CreatePatient: React.FC = () => {
+  return (
+    <AdminLayout>
+      <form>
+        <h2>Teste</h2>
+      </form>
+    </AdminLayout>
+  );
+};
+
+export default CreatePatient;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch } from "react-router-dom";
+import CreatePatient from "../pages/CreatePatient";
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
 import Profile from "../pages/Profile";
@@ -11,6 +12,7 @@ const Routes: React.FC = () => (
     <Route path="/" exact component={Login} />
     <Route path="/dashboard" component={Dashboard} isPrivate />
     <Route path="/profile/:id" component={Profile} isPrivate />
+    <Route path="/patient" component={CreatePatient} isPrivate />
   </Switch>
 );
 
