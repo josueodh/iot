32f36c41ce403368420aefd166441b819f48411f
Juan Oliveira <juan.neuroengenharia@gmail.com>
2021-03-12 13:08:50 -0300

    update profile_screen 2

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -3,4 +3,6 @@ import styled from "styled-components";
 
 export const Content = styled(Card)`
   width: 100%;
+  border-radius: 5px;
+  box-shadow: inset 0 0 1em rgb(248, 248, 248), 0 0 1em rgb(204, 204, 204);
 `;
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -3,6 +3,9 @@ import { PageHeader } from "antd";
 export const Title = styled(PageHeader)`
   border: 1px solid rgb(211, 214, 219);
   border-radius: 5px;
+  margin-bottom:20px;
+  
+  box-shadow: inset 0 0 0.5em rgb(248, 248, 248), 0 0 0.5em rgb(204, 204, 204);
 `;
 
 
